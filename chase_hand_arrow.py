eaa4acd7d5122ce31f047decadf2ed0e11b85411
triglan <glanb4277@gmail.com>
2023-10-01 20:17:06 +0900

    Drill 05 제출 2020182028이규원

--- chase_hand_arrow.py
+++ chase_hand_arrow.py
@@ -4,12 +4,10 @@ import random
 open_canvas()
 
 tuk_ground = load_image('TUK_GROUND.png')
-character = load_image('blueHeadCharacter.png')
-TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+character = load_image('animation_sheet.png')
+arrow = load_image('hand_arrow.png')
+TUK_WIDTH, TUK_HEIGHT = 800, 600
 
-running = True
-x = 800 // 2
-y = 600 // 2
 
 def handle_events():
     global running, x, y
@@ -18,38 +16,34 @@ def handle_events():
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_RIGHT:
-                anim = 1
-            elif event.key == SDLK_LEFT:
-                anim = 2
-            elif event.key == SDLK_UP:
-                anim = 3
-            elif event.key == SDLK_DOWN:
-                anim = 4
-            elif event.key == SDLK_ESCAPE:
-                running = False
-        elif event.type == SDL_KEYUP:
-            anim = 0
-frame = 0
+            running = False
 
+running = True
+x , y = 800 // 2, 600 // 2 # character coord
+px, py, ax, ay = 0, 0, random.randint(100, 700), random.randint(100, 700) # prevent, arrow coord
+frame = 0
+i = 0
 while running:
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    if anim == 0:
-        character.clip_draw(114, 560 - frame * 114, 114, 114, x, y)
-    elif anim == 1:
-        character.clip_draw(frame * 114 + 684, 560, 114, 114, x, y)
-        x += 5
-    elif anim == 2:
-        character.clip_composite_draw(frame * 114 + 684, 560, 114, 114, 0, 'h',  x, y, 114, 114)
-        x -= 5
-    elif anim == 3:
-        character.clip_draw(0 + frame * 115, 0, 114, 113, x, y)
-        y += 5
-    elif anim == 4:
-        character.clip_draw(0 + frame * 115, 0, 114, 113, x, y)
-        y -= 5
+    if frame == 0:
+        t = i / 100
+        x = (1 - t) * px + t * ax
+        y = (1 - t) * py + t * ay
+        i += 2
+        if i > 100:
+            ax, ay = random.randint(100, 700), random.randint(100, 700)
+            px, py = x ,y
+            i = 0
+
+    if px < ax:
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+    else:
+        character.clip_composite_draw(frame * 100, 100, 100, 100, 0, 'h', x, y, 100, 100)
+
+    arrow.draw(ax,ay)
     update_canvas()
     handle_events()
-    frame = (frame + 1) % 3
-    delay(0.05)
+    frame = (frame + 1) % 8
+
+close_canvas()
